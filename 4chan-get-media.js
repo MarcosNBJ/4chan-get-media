e57e5f02d62345416f9ad9fafb38356ac7f5ec6e
MarcosNBJ <marcosnery.comp@gmail.com>
2021-01-14 04:03:29 -0300

    Version 1.0.0

--- 4chan-get-media.js
+++ 4chan-get-media.js
@@ -1,1 +1,20 @@
-console.log("Setting up module");
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+exports.get_thread_media = (threadUrl) => {
+  const mediaList = [];
+  axios.get(threadUrl)
+      .then(function(response) {
+        const $ = cheerio.load(response.data);
+        $('div.postContainer').each((i, elem) => {
+          const media = $(elem).find('.fileThumb');
+          if (media.length) {
+            mediaList[i] = {
+              thumbnail: 'https:'+ media.find('img').attr('src'),
+              url: 'https:'+ media.attr('href'),
+            };
+          }
+        });
+      }, (error) => console.log(error) );
+  return mediaList;
+};
